36230afd3fddf1ecf9a165af22e9e1981078d1d6
Raihan Sk <raihank12656@gmail.com>
2025-01-20 14:55:39 +0530

    with animation

---
+++ lib/const/app_data.dart
@@ -0,0 +1,8 @@
+class AppData {
+  bool _isClient = true;
+  bool get isClient => _isClient;
+
+  void setIsClient(bool isClient) {
+    _isClient = isClient;
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Tidallal',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: AppColors.primary),
